cb49bc261438fcfc6221ddcf81ee76c165e86f61
Matvei <matveimalakhov@gmail.com>
2024-10-18 10:16:57 +0400

    Adding right input

--- VZN/src/App.tsx
+++ VZN/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css"
+import Input from "./components/input/Input"
 import './reset.css'
 
 export default function App() {
   return(
     <>
+    <Input legendText="Номер ВЗН*" required={true}/>
     </>
   )
 }
