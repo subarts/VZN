28c5bd37d48ebb7ea98f46a98fa55e28cbfeb9ec
subarts <subbota1991@gmail.com>
2024-10-18 13:00:46 +0400

    add  save the token and redirecting to the menu page

--- VZN/src/pages/auth/Auth.tsx
+++ VZN/src/pages/auth/Auth.tsx
@@ -2,7 +2,20 @@ import Footer from "../../components/footer/Footer"
 import logo from "../../assets/logo/Frame (5).svg"
 import Input from "../../components/input/Input"
 import style from "./Auth.module.css"
+import { useState } from "react"
+import { authorization } from "../../api/authorization"
+import { LogPass } from "../../types"
+
 const Auth = () => {
+  const [logInform, setLogInform] = useState<LogPass>({
+    login: "",
+    password: "",
+  })
+  function getAuthToken(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    authorization(logInform)
+  }
+
   return (
     <>
       <main className={style.main}>
@@ -10,8 +23,22 @@ const Auth = () => {
           <img src={logo} width={"50px"} height={"50px"} />
           <h1>OmpMobile</h1>
         </div>
-        <Input legendText="Логин" />
-        <Input legendText="Пароль" />
+        <form action="" onSubmit={getAuthToken}>
+          <Input
+            legendText="Логин"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setLogInform({ ...logInform, login: e.target.value })
+            }
+          />
+          <Input
+            legendText="Пароль"
+            type="password"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setLogInform({ ...logInform, password: e.target.value })
+            }
+          />
+          <button type="submit"> button</button>
+        </form>
       </main>
       <Footer />
     </>
--- VZN/src/types.ts
+++ VZN/src/types.ts
@@ -1,2 +1,4 @@
-
-
+export type LogPass = {
+  login: string
+  password: string
+}
