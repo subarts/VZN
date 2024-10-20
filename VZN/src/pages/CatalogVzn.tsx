680d857aa203627710d33bd1aaf9582d7de12965
bannikov_vlad <vladvannikov@gmail.com>
2024-10-20 23:06:28 +0400

    start doing on modalCreateVZN and infoAboutVZN

--- VZN/src/pages/CatalogVzn.tsx
+++ VZN/src/pages/CatalogVzn.tsx
@@ -1,62 +1,70 @@
-import Button from "../components/button/Button"
-import Footer from "../components/footer/Footer"
-import { Header } from "../components/header/Header"
-import ModalFilterVzn from "../components/modalFilterVzn/ModalFilterVzn"
-import OptionIcon from "../components/icons/OptionIcon"
-import PLusIcon from "../components/icons/PlusIcon"
-import Consigment from "../components/consigment/Consigment"
-import { useState } from "react"
-import { THeaderStyle } from "../types"
-import CloseIcon from "../components/icons/CloseIcon"
+import { useState } from 'react'
+import Button from '../components/button/Button'
+import Consigment from '../components/consigment/Consigment'
+import Footer from '../components/footer/Footer'
+import { Header } from '../components/header/Header'
+import CloseIcon from '../components/icons/CloseIcon'
+import OptionIcon from '../components/icons/OptionIcon'
+import PLusIcon from '../components/icons/PlusIcon'
+import ModalFilterVzn from '../components/modalFilterVzn/ModalFilterVzn'
+import ModalCreateVZN from '../components/modalCreateVZN/ModalCreateVZN'
+import { THeaderStyle } from '../types'
+
+type TVisibleModal = 'create' | 'search'
+
 const CatalogVzn = () => {
-  const [isVisibleModal, setIsVisibleModal] = useState(true)
-  const headerProps: THeaderStyle = isVisibleModal
-    ? { style: ["border", "justifyStartH1"] }
-    : { style: "" }
-  function headerRenderProps(): React.ReactNode {
-    return !isVisibleModal ? (
-      <h1>
-        ВЗН УП (Расход)
-        <div className="listButtons">
-          <Button
-            onClick={() => setIsVisibleModal(true)}
-            children={
-              <>
-                <OptionIcon className="" />
-                Поиск
-              </>
-            }
-            size={"Small"}
-            className={"smallButtonText"}
-          />
-          <Button
-            children={
-              <>
-                <PLusIcon className={""} />
-                Создать
-              </>
-            }
-            size={"Small"}
-            className={"smallButtonText"}
-          />
-        </div>
-      </h1>
-    ) : (
-      <h1>
-        <CloseIcon className="" onClick={() => setIsVisibleModal(false)} />
-        Фильтр ВЗН УП
-      </h1>
-    )
-  }
-  return (
-    <>
-      <Header render={() => headerRenderProps()} headerProps={headerProps} />
+	const [visibleModalType, setVisibleModalType] = useState<TVisibleModal | ''>(
+		''
+	)
 
-      {isVisibleModal ? <ModalFilterVzn /> : <Consigment />}
+	const headerProps: THeaderStyle = visibleModalType
+		? { style: ['border', 'justifyStartH1'] }
+		: { style: '' }
 
-      <Footer />
-    </>
-  )
+	function headerRenderProps(): React.ReactNode {
+		return !visibleModalType ? (
+			<h1>
+				ВЗН УП (Расход)
+				<div className='listButtons'>
+          <Button
+            onClick={() => setVisibleModalType('search')}
+            size={'Small'}
+						className={'smallButtonText'}
+          >
+						<>
+							<OptionIcon className='' />
+							Поиск
+						</>
+          </Button>
+          
+          <Button
+            onClick={() => setVisibleModalType('create')}
+            size={'Small'}
+						className={'smallButtonText'}
+          >
+						<>
+              <PLusIcon />
+              Создать
+						</>
+					</Button>
+				</div>
+			</h1>
+		) : (
+			<h1>
+				<CloseIcon className='' onClick={() => setVisibleModalType('')} />
+				{visibleModalType === 'search' ? 'Фильтр ВЗН УП' : 'Создание ВЗН (Расход)'}
+			</h1>
+		)
+	}
+	return (
+		<>
+			<Header render={() => headerRenderProps()} headerProps={headerProps} />
+
+      {visibleModalType === 'search' ? <ModalFilterVzn /> : visibleModalType === 'create' ? <ModalCreateVZN /> : <Consigment />}
+
+			<Footer />
+		</>
+	)
 }
 
 export default CatalogVzn
