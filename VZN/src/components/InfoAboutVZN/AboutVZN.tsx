b71cc4e3e749dbad8fba841d9beb5837164692d5
subarts <subbota1991@gmail.com>
2024-10-23 00:26:52 +0400

    change route numberVzn to Code Vzn, add save data to store

--- VZN/src/components/InfoAboutVZN/AboutVZN.tsx
+++ VZN/src/components/InfoAboutVZN/AboutVZN.tsx
@@ -1,28 +1,47 @@
 import { useParams } from "react-router-dom"
 import Button from "../button/Button"
 import style from "./aboutVZN.module.css"
-import { InfoVzn } from "../../api/InfoVzn"
 import { useStore } from "../../store/Store"
+import { useEffect, useState } from "react"
+import { TListRequest } from "../../types"
+import { InfoCardsVzn } from "../../api/InfoCardsVzn"
+
 export const AboutVZN = () => {
   const listVzn = useStore((state) => state.listVzn)
-  const numberVzn = useParams()
+  const numberUnicCodeVzn = useParams()
   const bodyRequest = {
-    WsInplantCode: numberVzn,
+    WsInplantCode: listVzn.Num,
   }
-  InfoVzn(bodyRequest)
+  const { findDivison } = useStore((state) => state)
+  const [cards, setCards] = useState([])
+  const requestVzn = async (body: TListRequest): Promise<void> => {
+    const result = await InfoCardsVzn(bodyRequest)
+    setCards(result)
+  }
+  useEffect(() => {
+    requestVzn(bodyRequest)
+  }, [])
+  /*  const requestCardsVzn = async (): Promise<void> => {
+    const result = await CardsVzn(bodyRequest)
+  }
+  requestCardsVzn() */
+  const itemVzn = listVzn.filter(
+    (el) => el.Code == numberUnicCodeVzn.numberUnicCodeVzn
+  )
 
-  const itemVzn = listVzn.filter((el) => el.Num == numberVzn.numberVzn)[0]
-  console.log(itemVzn)
+  const senderName = findDivison(itemVzn[0].Sender)
+  const receiverName = findDivison(itemVzn[0].Receiver)
+
   return (
     <main className={style.main}>
       <section className={style.info}>
         <p>
           <span className={style.bold}>Отправитель: </span>
-          {itemVzn.Sender} / участок Цеха 01
+          {senderName[0].name}/ участок Цеха 01
         </p>
         <p>
           <span className={style.bold}>Получатель: </span>
-          {itemVzn.Receiver} / участок Цеха 02
+          {receiverName[0].name} / участок Цеха 02
         </p>
         <p>
           <span className={style.bold}>Статус: </span>НеУтв
@@ -37,7 +56,17 @@ export const AboutVZN = () => {
           <Button size="Small">Создать</Button>
         </div>
 
-        <div className={style.list}>Cписок пуст...</div>
+        <div className={style.list}>
+          {cards.map((el, index) => {
+            return (
+              <div key={index}>
+                <p>принято:{el.ArrivalQty}</p>
+                <p>отпущено :{el.LeaveQty}</p>
+                <p>{el.Code}</p>
+              </div>
+            )
+          })}
+        </div>
       </section>
     </main>
   )
--- VZN/src/components/consigment/Consigment.tsx
+++ VZN/src/components/consigment/Consigment.tsx
@@ -2,7 +2,6 @@ import styles from "./consigment.module.css"
 import { Link } from "react-router-dom"
 import { TListVznPropsItem } from "../../types"
 import { useStore } from "../../store/Store"
-
 type listVznProps = {
   listVzn: TListVznPropsItem[]
 }
@@ -10,23 +9,26 @@ type listVznProps = {
 const Consigment: React.FC<listVznProps> = ({ listVzn }) => {
   const isObject: TListVznPropsItem[] = listVzn ? listVzn : []
   const isEpmtyObj = Object.entries(isObject).length > 0
+  const { findDivison } = useStore((state) => state)
   return (
     <div className={styles.consigmentList}>
       {isEpmtyObj ? (
         listVzn?.map((el: TListVznPropsItem, index: number) => {
+          const senderName = findDivison(el.Sender)
+          const receiverName = findDivison(el.Receiver)
           return (
             <Link
-              to={el.Num.toString()}
+              to={el.Code.toString()}
               className={styles.consigmentItem}
               key={index}
             >
               <h4>ВЗН №{el.Num}</h4>
               <ul className={styles.consigmentItemDescript}>
                 <li>
-                  Отправитель: <span>{el.Sender}</span>
+                  Отправитель: <span>{senderName[0].name}</span>
                 </li>
                 <li>
-                  Получатель: <span>{el.Receiver}</span>
+                  Получатель: <span>{receiverName[0].name}</span>
                 </li>
                 <li>
                   Дата выдачи: <span>{el.LeaveMoveDate}</span>
