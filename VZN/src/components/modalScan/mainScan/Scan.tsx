62ac1348846a708f07e8854d72c142cecd0e8cf9
Matvei <matveimalakhov@gmail.com>
2024-10-24 16:57:11 +0400

    Adding fetch for scan

--- VZN/src/components/modalScan/mainScan/Scan.tsx
+++ VZN/src/components/modalScan/mainScan/Scan.tsx
@@ -1,36 +1,47 @@
 import { SetStateAction, useState } from 'react';
-import Button from '../../button/button'
-import Input from '../../input/Input'
-import styles from '../mainScan/scan.module.css'
+import Button from '../../button/Button';
+import Input from '../../input/Input';
+import styles from '../mainScan/scan.module.css';
 import Select from '../../select/Select';
+import { BoTypes } from '../../../api/BoTypes';
 
 export const Scan = () => {
-    const [inputValue, setInputValue] = useState('');
-    const isButtonDisabled = inputValue.length < 10
+    const [inputValue, setInputValue] = useState<string>('');
+    const isButtonDisabled = inputValue.length < 10;
 
     const handleInputChange = (e: { target: { value: SetStateAction<string>; }; }) => {
         setInputValue(e.target.value);
     };
 
-    return(
+    const fetchData = async () => {
+        const data = await BoTypes();
+        console.log(data)
+    };
+
+    fetchData();
+
+    return (
         <main className={styles.main}>
             <p className={styles.text}>Сканируйте штрихкод с номером объекта или введите его вручную.</p>
             <Input 
                 placeHolder={``}
-				legendText='Номер объекта'
-				type='text'
+                legendText='Номер объекта'
+                type='text'
                 value={inputValue}
-                onChange={handleInputChange}
+                onChange={handleInputChange} 
+                required={false} 
+                className={''} 
+                disabled={false}
             />
             <Select
-				legend='Тип объекта БО'
+                legend='Тип объекта БО'
                 disabled={inputValue.length < 10}
                 className={`${styles.selectFieldset} ${styles.selectFilter} ${inputValue.length < 10 ? styles.disabledSelect : styles.selectFieldset}`}
             />
             <div className={styles.buttons}>
-                <Button disabled={isButtonDisabled} className={isButtonDisabled ? 'button:disabled' : ''} type="submit" color='Blue'  size='Regular' children='Перейти'/>
-                <Button type='submit' color='TransparentWithBorder' children='Отмена'/>
+                <Button disabled={isButtonDisabled} className={isButtonDisabled ? 'button:disabled' : ''} type="submit" color='Blue' size='Regular' children='Перейти' />
+                <Button type='submit' color='TransparentWithBorder' children='Отмена' />
             </div>
         </main>
-    )
-}
+    );
+};
