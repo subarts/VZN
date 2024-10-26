8506dc5196d9874ee61849c535fc5920d8424a9c
subarts <subbota1991@gmail.com>
2024-10-26 09:57:51 +0400

    fix h1 modalScan

--- VZN/src/components/modalScan/ModalScan.tsx
+++ VZN/src/components/modalScan/ModalScan.tsx
@@ -3,30 +3,29 @@ import Footer from "../footer/Footer"
 import { Header } from "../header/Header"
 import CloseIcon from "../icons/CloseIcon"
 import { Scan } from "./mainScan/Scan"
-import styles from '../modalScan/modalScan.module.css'
+import styles from "../modalScan/modalScan.module.css"
 import { Link } from "react-router-dom"
 
 const ModalScan = () => {
-  const headerProps: THeaderStyle ={ style: ["border", "justifyStartH1"] }
+  const headerProps: THeaderStyle = { style: ["border", "justifyStartH1"] }
 
- 
   function headerRenderProps(): React.ReactNode {
     return (
-      <div className={styles.header}>
-        <Link to='/menu'>
-          <CloseIcon className='' />
+      <h1 className={styles.header}>
+        <Link to="/menu">
+          <CloseIcon className="" />
         </Link>
-        <h1>Сканировать номер объекта</h1>
-      </div>
-  );
-  
-    return (
-      <>
-        <Header render={() => headerRenderProps()} headerProps={headerProps}  />
-        <Scan />
-        <Footer />
-      </>
+        Сканировать номер объекта
+      </h1>
     )
   }
-  
-  export default ModalScan
+  return (
+    <>
+      <Header render={() => headerRenderProps()} headerProps={headerProps} />
+      <Scan />
+      <Footer />
+    </>
+  )
+}
+
+export default ModalScan
